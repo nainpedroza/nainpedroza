39a629d1030a7bd61c9929689e584e1d2a43c658
nainpedroza <88448255+nainpedroza@users.noreply.github.com>
2021-08-04 16:52:12 -0500

    Create Chap3-E2.js

---
+++ Set_0/Chap3-E2.js
@@ -0,0 +1,10 @@
+function add() {
+  let sum = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+
+var answer2 = add(1,2) + add(1,2,4,6,7);
+answer2; //23
