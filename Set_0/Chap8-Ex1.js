1cf5990dfb3becc86e70d186ff715dd7c27d2c47
nainpedroza <88448255+nainpedroza@users.noreply.github.com>
2021-08-04 17:22:46 -0500

    Create Chap8-Ex1.js

---
+++ Set_0/Chap8-Ex1.js
@@ -0,0 +1,14 @@
+var r_text = new Array ();
+r_text[0] = "All the leaves are brown";
+r_text[1] = "And the sky is grey";
+r_text[2] = "I've been for a walk";
+r_text[3] = "On a winter's day";
+r_text[4] = "I'd be safe and warm";
+r_text[5] = "If I was in L.A.";
+r_text[6] = "California dreaming, On such a winter's day";
+
+ setInterval(function(){   
+   var i = Math.floor(7*Math.random())
+    console.log(r_text[i]); 
+
+ }, 1000*60);
