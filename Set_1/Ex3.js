8a09867ebe45370b221302725a6ada18022a92e5
nainpedroza <88448255+nainpedroza@users.noreply.github.com>
2021-08-04 16:39:27 -0500

    Create Ex3.js

---
+++ Set_1/Ex3.js
@@ -0,0 +1,7 @@
+function display(textcolor ="#FF00FF",size = "x-large",bgcolor="blue" )//default colors are set
+{
+var col=document.getElementById("elementId");
+col.style.color=textcolor;
+col.style.fontSize = size;
+col.style.backgroundColor = bgcolor;
+}
