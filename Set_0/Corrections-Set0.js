6e7ddd4d997d3e5a2f30ea627809bb7b96a95c94
nainpedroza <88448255+nainpedroza@users.noreply.github.com>
2021-08-17 12:53:28 -0500

    Update Corrections-Set0.js

--- Set_0/Corrections-Set0.js
+++ Set_0/Corrections-Set0.js
@@ -91,5 +91,82 @@ function dataType(array){
 
 dataType(1,6.2123123,"pi*2",[function(){}, 1], {}, function () {}); // number, float, string, array, object, function
 
+//Ex5
+function distance(array){
+  if(arguments.length<3){
+    return "Insufficient parameters"
+  }
+  else if(arguments.length<5){
+    var v = [0,0,0,0];
+    for (let i=0; i<5;i++){
+        v[i] = arguments[i];
+    }
+    return Math.sqrt((v[2]-v[0])**2+(v[3]-v[1])**2);
+  }
+  else if(arguments.length<7){
+    var v = [0,0,0,0,0,0];
+    for (let i=0; i<7; i++){
+        v[i] = arguments[i];
+    }
+    return Math.sqrt((v[3]-v[0])**2+(v[4]-v[1])**2+(v[5]-v[2])**2);
+  }
+}
+var x1 = 1, y1 = 2, z1 = 1;
+var x2 = 2, y2 = 2, z2 = 4;
+var delta1 = distance (x1, y1, x2, y2);
+delta1; //1
+var delta2 = distance (x1, y1, z1, x2, y2, z2);
+delta2; //3.1622776601683795
+distance (x1, x2); //Insufficient parameters
 
-
+//Ex6
+function distance2(array){
+  var p = [];
+  if(typeof array == 'object'){
+      if (arguments.length==2){
+    if (arguments[0].length == arguments[1].length){
+        for (var i = 0; i < arguments.length; i++) {
+          for (var j = 0; j< arguments.length; j++){
+            p.push(arguments[i][j]);
+          }
+        }
+      return Math.sqrt((p[2]-p[0])**2+(p[3]-p[1])**2);
+    }
+    else{
+      console.error('Incompatible data points');
+    }
+  }
+  else if(arguments.length == 3){
+    if((arguments[0].length == arguments[1].length) && (arguments[2].length == arguments[1].length)){
+      for(var i = 0; i < arguments.length; i++){
+        for (var j = 0; j < arguments.length; j++){
+          p.push(arguments[i][j]);
+        }
+      }
+    }
+    return Math.sqrt((p[3]-p[0])**2+(p[4]-p[1])**2+(p[5]-p[2])**2);
+  }
+    else{
+      console.error('Incompatible data points');
+    }
+  }
+  else{
+   if(arguments.length<3){
+      console.error('Insufficient parameters');
+  }
+  else if(arguments.length<5){
+    var v = [0,0,0,0];
+    for (let i=0; i<5;i++){
+        v[i] = arguments[i];
+    }
+    return Math.sqrt((v[2]-v[0])**2+(v[3]-v[1])**2);
+  }
+  else if(arguments.length<7){
+    var v = [0,0,0,0,0,0];
+    for (let i=0; i<7; i++){
+        v[i] = arguments[i];
+    }
+    return Math.sqrt((v[3]-v[0])**2+(v[4]-v[1])**2+(v[5]-v[2])**2);
+    }
+  }
+}
