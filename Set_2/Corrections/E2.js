6c940cff7fe653af3188dd167ad02d88ca0583cf
nainpedroza <88448255+nainpedroza@users.noreply.github.com>
2021-08-20 10:56:23 -0500

    Create E2.js

---
+++ Set_2/Corrections/E2.js
@@ -0,0 +1,17 @@
+//2.A
+console.log(/(january|february|march|april|may|june|july|august|september|october|november|december) \d{2},\ \d{4}$/gmi.test("January 11, 2001")); //True
+
+//2.E
+var string = "Bee zapp Crow Eagle Zorro mouse Ape you"
+var expression = /\b[b-y]+\b/gmi;
+var array = string.match(expression);
+console.log(array); // [ 'Bee', 'Crow', 'mouse', 'you' ]
+
+//2.F
+var stri = "Is <b>4 < -1/12</b> true? The <b>answer</b> will <em>surprise</em> you. <b>#</b> "
+function checkTags(string){
+  var expression = /<(\w+)>(\s*[\wW]*\s*|\s*[\wW]*\s*|\d*\W*\d*\/?\d*|\W*\w*)<\/(\1)>/gmi;
+  var array = string.match(expression);
+  console.log(array);
+}
+checkTags(stri); //[ '<b>4 < -1/12</b>', '<b>answer</b>', '<em>surprise</em>', '<b>#</b>' ]
