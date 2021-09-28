cd8e1e29cbb049fec07ab829a53f561b9aa02192
nainpedroza <88448255+nainpedroza@users.noreply.github.com>
2021-09-28 18:52:51 -0500

    Create gridBuilder.js

---
+++ Set_0/Corrections/Chapter14-1/gridBuilder.js
@@ -0,0 +1,19 @@
+const container = document.getElementById("container");
+const entry = document.getElementsByTagName('input')
+function makeRows() {
+    var rows = document.getElementById("heightValue").value;
+    var cols = document.getElementById("widthValue").value;
+  container.innerHTML = ''
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+function reset() {
+   container.innerHTML = ''
+   entry[0].value = '1';
+   entry[1].value = '1';
+  }
