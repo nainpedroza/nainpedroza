97b30bf0507f387b6b01a87ddb05f8b0f8d4e16e
nainpedroza <88448255+nainpedroza@users.noreply.github.com>
2021-10-11 15:13:26 -0500

    Update and rename C5-Ex1v2.js to C5-Ex1V3.js

--- Set_0/Corrections/C5-Ex1V3.js
+++ Set_0/Corrections/C5-Ex1V3.js
@@ -1,8 +1,9 @@
 class Client{
-    constructor(name, cash, account){
+    constructor(name, cash, account, bankName){
         this.name = name;
         this.cash = cash;
         this.account = account;
+      this.bankName = bankName;
     }
     deposit(amount, receptorAccount){
         if (amount < 0){
@@ -17,13 +18,13 @@ class Client{
     retrieve(amount){
         this.cash += bank.retrieve(amount, this.account)
     }
-    showBalance(){
-        return bank.showBalance(this.account);
+    showBalance(bankName){
+        return bank.showBalance(this.account) + ' in the bank: '+ this.bankName;
     }
 }
 
 class Bank{
-    constructor(name, accounts){
+    constructor(name,accounts){
         this.name = name;
         this.accounts = accounts;
     }
@@ -37,15 +38,13 @@ class Bank{
     retrieve(amount, accountNumber){
       let fromThisClient = this.getClient(accountNumber);
       if (amount < 0){
-        console.log('You can not deposit a negative amount');
-        return 0
+        throw ('You can not deposit a negative amount');
       }
       if (fromThisClient['bankBalance'] >= amount){
         fromThisClient['bankBalance'] -= amount;
         return amount;
       }
-      console.log('You do not have enough money to retrieve');
-      return 0;
+      throw ('You do not have enough money to retrieve');
     }
     deposit(amount, toWhichAccount){
         let toWhichClient = this.getClient(toWhichAccount);
@@ -56,21 +55,20 @@ class Bank{
       return balanceClient['bankBalance'];
     }
 }
-
-
-function addNewBankClient(name, accountNumber, bankMoney, cash){
-    let addNewClient = new Client(name, cash, accountNumber);
+function addNewBankClient(name, accountNumber, bankMoney, cash, bankName){
+    let addNewClient = new Client(name, cash, accountNumber, bankName);
     bank.addClient({
       'accountNumber':accountNumber, 
       'owner':name, 
-      'bankBalance':bankMoney
+      'bankBalance':bankMoney,
+      'bankName': bankName
     });
     return addNewClient;
 }
 //Creation of a bank into Bank's class 
-let bank = new Bank('BBVA', [{'accountNumber':0, 'owner':'Nain', 'bankBalance': 10000}]);
+let bank = new Bank('BBVA',[{'accountNumber':0, 'owner':'Nain', 'bankBalance': 10000}]);
 //Add this new client to Clients' class
-let Nain = new Client('Nain', 5000, 0); // 5000, cash (not deposited)
+let Nain = new Client('Nain', 5000, 0,'BBVA'); // 5000, cash (not deposited)
 console.log(Nain.showBalance()); //10000
 console.log("Nain's balance after 5000 deposit")
 Nain.deposit(5000,0);
@@ -79,12 +77,13 @@ console.log(Nain.cash); // 0
 console.log("Nain's balance after 1000 retrieved")
 Nain.retrieve(1000)
 console.log(Nain.showBalance()); //14000
-let Marco = addNewBankClient('Marco', 1, 9000, 1000); //9000  money to open the account, 1000 in cash
+let Marco = addNewBankClient('Marco', 1, 9000, 1000,'HSBC'); //9000  money to open the account, 1000 in cash
 console.log("Marco's balance")
 console.log(Marco.showBalance()); //9000 (money used to open the account)
-Marco.retrieve(100);
+Marco.retrieve(-100);
 console.log("Marco's balance after 100 retrieved")
 console.log(Marco.showBalance()); //8900
 Marco.deposit(800,1)
 console.log("Nain's balance after 500 deposit")
 console.log(Marco.showBalance()); //9700
+Marco.retrieve(-100); //Error: You can not retrieve a negative amount
